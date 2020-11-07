56fb0de61d372ee1600404e5a682f61050b6e608
Carly <carlyne.lapierre@gmail.com>
2020-11-07 22:51:58 +0100

    refacto des classes

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,6 @@
-const players = [...document.querySelectorAll('.card')];
-const activePlayers = players
-						.filter(player => !player.classList.contains('disabled'))
-						.map(activePlayer => activePlayer.getAttribute('id'));
+const activePlayersId =['player-one', 'player-two'];
 
-const team = [];
-
-class CharacterSelection {
+/*class CharacterSelection {
 	constructor(playerId) {
 		this.playerId = playerId;
 	}
@@ -19,40 +14,90 @@ class CharacterSelection {
 	}
 }
 
-class Player extends CharacterSelection {
-	constructor(playerId) {
-		super(playerId);
-	}
-}
+*/
 
-class Game {
-	constructor(playerId) {
+class PlayerConfiguration {
+	constructor(playerId, gameInterface) {
 		this.playerId = playerId;
-		this.trigger();
+		this.gameInterface = gameInterface;
+		this.characterId = 'Punk';
 	}
 
-	trigger() {
-		const character = new CharacterSelection(this.playerId);
-
+	confirmPlayerEvent() {
 		const addBtn = document.querySelector('#' + this.playerId + ' .add');
+
+		addBtn.addEventListener('click', () => {
+			this.gameInterface.confirmedCharacters = this.characterId;
+		})
+
+		return this;
+	}
+
+	editCharacterEvents(){
 		const nextBtn = document.querySelector('#' + this.playerId + ' .select-next');
 		const prevBtn = document.querySelector('#' + this.playerId + ' .select-prev');
 
 		nextBtn.addEventListener('click', () => {
-			character.changeNext();
+			console.log('next character');
 		})
 
 		prevBtn.addEventListener('click', () => {
-			character.changePrev();
+			console.log('prev character');
 		})
 
-		addBtn.addEventListener('click', () => {
-			team.push(new Player(this.playerId));
-			console.log(team);
-		})
+		return this;
+	};
+
+	addEvents() {
+		return this.confirmPlayerEvent().editCharacterEvents();
 	}
 }
 
-activePlayers.forEach(player => {
-	new Game(player);
-})
+
+class GameInterface {
+	constructor(activePlayersId) {
+		this._activePlayersId = activePlayersId;
+		this._confirmedCharacters = [];
+
+		this.addPlayerConfiguration();
+	}
+
+	get activePlayersId() {
+		return this._activePlayersId;
+	}
+
+	get confirmedCharacters() {
+		return this._confirmedCharacters;
+	}
+
+	set activePlayersId(newActivePlayersId) {
+		this._activePlayersId = newActivePlayersId;
+		return this;
+	}
+
+	set confirmedCharacters(newConfirmedCharacters) {
+		this._confirmedCharacters.push(newConfirmedCharacters);
+
+		return this;
+	}
+
+	addPlayerConfiguration() {
+		this.activePlayersId.forEach(playerId => {
+			this.updateInterface(playerId);
+			
+			const playerConfiguration = new PlayerConfiguration(playerId, this);
+			playerConfiguration.addEvents();
+		});
+
+		return this;
+	}
+
+	updateInterface(playerId) {
+		const card = document.getElementById(playerId);
+		card.classList.remove('disabled');
+
+		return this;
+	}
+}
+
+new GameInterface(activePlayersId);
